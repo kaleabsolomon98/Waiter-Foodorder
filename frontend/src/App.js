a16e31da1863040e35b2587faf8b77033eafbf0c
kaleab solomon <kaleabsolomon98@gmail.com>
2024-10-21 13:39:10 +0300

    Adding a background image to a header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Placeorder from './pages/PlaceOrder/Placeorder'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
 
 
 const App = () => {
@@ -9,8 +11,8 @@ const App = () => {
     <div className='app'>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
         <Route path='/order' element={<Placeorder />} />
       </Routes>
     </div>
