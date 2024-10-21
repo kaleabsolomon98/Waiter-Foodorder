36ec45ea635af7619100a049883b48cd07d5db88
kaleab solomon <kaleabsolomon98@gmail.com>
2024-10-21 13:01:55 +0300

    Adding router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Placeorder from './pages/PlaceOrder/Placeorder'
 
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<Placeorder />} />
+      </Routes>
     </div>
   )
 }
