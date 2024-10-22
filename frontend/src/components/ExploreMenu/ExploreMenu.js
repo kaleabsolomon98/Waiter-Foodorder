ed12bc6525dc77805f49c1c4fe65cb3954a3688d
kaleab solomon <kaleabsolomon98@gmail.com>
2024-10-22 15:02:52 +0300

    Adding cart

--- frontend/src/components/ExploreMenu/ExploreMenu.js
+++ frontend/src/components/ExploreMenu/ExploreMenu.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './ExploreMenu.css'
 import { food_list, menu_list } from '../../assets/frontend_assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
+
+    useEffect(() => {
+        console.log('Current category updated:', category); // Logs after every state update
+    }, [category]); // This effect runs whenever `category` changes
+
     return (
         <div className='explore-menu' id='explore-menu'>
             <h1>Explore our menu</h1>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingridents and culinary expertise.our mission is to satisfy your dining experience,one delicious meal at a time</p>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your dining experience, one delicious meal at a time.</p>
             <div className='explore-menu-list'>
                 {menu_list.map((item, index) => {
-                    return (<div key={index} className='explore-menu-list-item'>
-                        <img src={item.menu_image} alt="" />
-                        <p>{item.menu_name}</p>
-                    </div>)
+                    return (
+                        <div
+                            onClick={() => setCategory(prev => prev === item.menu_name ? 'All' : item.menu_name)}
+                            key={index}
+                            className='explore-menu-list-item'
+                            >
+                            <img
+                                className={category === item.menu_name ? 'active' : ''}
+                                src={item.menu_image}
+                                alt={item.menu_name}
+                            />
+                            <p>{item.menu_name}</p>
+                        </div>
+                    )
                 })}
             </div>
             <hr />
@@ -20,4 +35,4 @@ const ExploreMenu = () => {
     )
 }
 
-export default ExploreMenu
+export default ExploreMenu;
