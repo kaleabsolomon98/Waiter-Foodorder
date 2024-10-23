fdcff085afcde79258e429efd14e6a3910099fdb
kaleab solomon <kaleabsolomon98@gmail.com>
2024-10-23 17:16:39 +0300

    Adding menu registration form

--- frontend/src/pages/MenuRegistration/MenuRegistration.js
+++ frontend/src/pages/MenuRegistration/MenuRegistration.js
@@ -1,124 +1,151 @@
-import './MenuRegistration.css'; 
-import {useState} from "react"; 
+import './MenuRegistration.css';
+import { useState } from "react";
 // import {validateEmail} from "../src/utils"; 
- 
-const PasswordErrorMessage = () => { 
- return ( 
-   <p className="FieldError">Password should have at least 8 characters</p> 
- ); 
-}; 
- 
-function MenuRegistration() { 
- const [firstName, setFirstName] = useState(""); 
- const [lastName, setLastName] = useState(""); 
- const [email, setEmail] = useState(""); 
- const [password, setPassword] = useState({ 
-   value: "", 
-   isTouched: false, 
- }); 
- const [role, setRole] = useState("role"); 
 
- const getIsFormValid = () => { 
-   return ( 
-     firstName && 
-    //  validateEmail(email) && 
-     password.value.length >= 8 && 
-     role !== "role" 
-   ); 
- }; 
- 
- const clearForm = () => { 
-   setFirstName(""); 
-   setLastName(""); 
-   setEmail(""); 
-   setPassword({ 
-     value: "", 
-     isTouched: false, 
-   }); 
-   setRole("role"); 
- }; 
- 
- const handleSubmit = (e) => { 
-   e.preventDefault(); 
-   alert("Account created!"); 
-   clearForm(); 
- }; 
- 
- return ( 
-   <div className="App"> 
-     <form onSubmit={handleSubmit}> 
-       <fieldset> 
-         <h2>Sign Up</h2> 
-         <div className="Field"> 
-           <label> 
-             First name <sup>*</sup> 
-           </label> 
-           <input 
-             value={firstName} 
-             onChange={(e) => { 
-               setFirstName(e.target.value); 
-             }} 
-             placeholder="First name" 
-           /> 
-         </div> 
-         <div className="Field"> 
-           <label>Last name</label> 
-           <input 
-             value={lastName} 
-             onChange={(e) => { 
-               setLastName(e.target.value); 
-             }} 
-             placeholder="Last name" 
-           /> 
-         </div> 
-         <div className="Field"> 
-           <label> 
-             Email address <sup>*</sup> 
-           </label> 
-           <input 
-             value={email} 
-             onChange={(e) => { 
-               setEmail(e.target.value); 
-             }} 
-             placeholder="Email address" 
-           /> 
-         </div> 
-         <div className="Field"> 
-           <label> 
-             Password <sup>*</sup> 
-           </label> 
-           <input 
-             value={password.value} 
-             type="password" 
-             onChange={(e) => { 
-               setPassword({ ...password, value: e.target.value }); 
-             }} 
-             onBlur={() => { 
-               setPassword({ ...password, isTouched: true }); 
-             }} 
-             placeholder="Password" 
-           /> 
-           {password.isTouched && password.value.length < 8 ? ( 
-             <PasswordErrorMessage /> 
-           ) : null} 
-         </div> 
-         <div className="Field"> 
-           <label> 
-             Role <sup>*</sup> 
-           </label> 
-           <select value={role} onChange={(e) => setRole(e.target.value)}> 
-             <option value="role">Role</option> 
-             <option value="individual">Individual</option> 
-             <option value="business">Business</option> 
-           </select> 
-         </div> 
-         <button type="submit" disabled={!getIsFormValid()}> 
-           Create account 
-         </button> 
-       </fieldset> 
-     </form> 
-   </div> 
- ); 
-} 
+const PasswordErrorMessage = () => {
+    return (
+        <p className="FieldError">Password should have at least 8 characters</p>
+    );
+};
+
+function MenuRegistration() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState({
+        value: "",
+        isTouched: false,
+    });
+    const [role, setRole] = useState("role");
+    const [isFridgeItem, setIsFridgeItem] = useState("");
+
+    const getIsFormValid = () => {
+        return (
+            firstName &&
+            //  validateEmail(email) && 
+            // password.value.length >= 8 &&
+            role !== "role"
+        );
+    };
+
+    const clearForm = () => {
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPassword({
+            value: "",
+            isTouched: false,
+        });
+        setRole("role");
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("Account created!");
+        clearForm();
+    };
+
+    return (
+        <div className="App">
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <h2 className='h2'>Add New Menu</h2>
+                    <div className="Field">
+                        <label>
+                            Select Printer <sup>*</sup>
+                        </label>
+                        <select value={role} onChange={(e) => setRole(e.target.value)}>
+                            <option value="role">Role</option>
+                            <option value="individual">Individual</option>
+                            <option value="business">Business</option>
+                        </select>
+                    </div>
+                    <div className="Field">
+                        <label>Is Fridge Item?</label>
+                        <div className='RadioButton'>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="yes"
+                                    checked={isFridgeItem === "yes"}
+                                    onChange={() => setIsFridgeItem("yes")}
+                                />
+                                Yes
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="no"
+                                    checked={isFridgeItem === "no"}
+                                    onChange={() => setIsFridgeItem("no")}
+                                />
+                                No
+                            </label>
+                        </div>
+                    </div>
+                    <div className="Field">
+                        <label>
+                            Category <sup>*</sup>
+                        </label>
+                        <select value={role} onChange={(e) => setRole(e.target.value)}>
+                            <option value="role">Role</option>
+                            <option value="individual">Individual</option>
+                            <option value="business">Business</option>
+                        </select>
+                    </div>
+                    <div className="Field">
+                        <label>
+                            Sub Category
+                        </label>
+                        <select value={role} onChange={(e) => setRole(e.target.value)}>
+                            <option value="role">Role</option>
+                            <option value="individual">Individual</option>
+                            <option value="business">Business</option>
+                        </select>
+                    </div>
+                    <div className="Field">
+                        <label>
+                            Menu Name <sup>*</sup>
+                        </label>
+                        <input
+                            value={firstName}
+                            onChange={(e) => {
+                                setFirstName(e.target.value);
+                            }}
+                            placeholder="Menu name"
+                        />
+                    </div>
+                    <div className="Field">
+                        <label>
+                            Price  <sup>*</sup>
+                        </label>
+                        <input
+                            value={lastName}
+                            onChange={(e) => {
+                                setLastName(e.target.value);
+                            }}
+                            placeholder="Price"
+                        />
+                    </div>
+                    <div className="Field">
+                        <label>
+                            Description
+                        </label>
+                        <input
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                            placeholder="Description"
+                        />
+                    </div>
+                    <button type="submit" disabled={!getIsFormValid()}>
+                        Create account
+                    </button>
+                </fieldset>
+            </form>
+        </div>
+    );
+}
 
 export default MenuRegistration; 
