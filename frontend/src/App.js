07e99792e80e46b08f2daabee0f4e92970910a89
kaleab solomon <kaleabsolomon98@gmail.com>
2024-10-23 16:36:59 +0300

    Adding menu registration to the nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,25 @@ import Cart from './pages/Cart/Cart';
 import Login from './pages/Login/Login';
 import { StoreContext } from './context/StoreContext';
 import './App.css';
+import MenuRegistration from './pages/MenuRegistration/MenuRegistration';
 
 const App = () => {
-    const { isLoggedIn } = useContext(StoreContext);
+  const { isLoggedIn } = useContext(StoreContext);
 
-    return (
-        <div className={`${!isLoggedIn ? 'full-width' : 'app'}`}>
-            {isLoggedIn && <Navbar />}
-            <Routes>
-                {/* Redirect to home if logged in and tries to access login page */}
-                <Route path='/' element={isLoggedIn ? <Navigate to="/home" replace /> : <Login />} />
-                {/* Ensure that home and other routes are accessible only if not logged in */}
-                <Route path='/home' element={isLoggedIn ? <Home /> : <Navigate to="/" replace />} />
-                <Route path='/cart' element={isLoggedIn ? <Cart /> : <Navigate to="/" replace />} />
-                <Route path='/order' element={isLoggedIn ? <Placeorder /> : <Navigate to="/" replace />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className={`${!isLoggedIn ? 'full-width' : 'app'}`}>
+      {isLoggedIn && <Navbar />}
+      <Routes>
+        {/* Redirect to home if logged in and tries to access login page */}
+        <Route path='/' element={isLoggedIn ? <Navigate to="/home" replace /> : <Login />} />
+        {/* Ensure that home and other routes are accessible only if not logged in */}
+        <Route path='/home' element={isLoggedIn ? <Home /> : <Navigate to="/" replace />} />
+        <Route path='/menu' element={isLoggedIn ? <MenuRegistration /> : <Navigate to="/" replace />} />
+        <Route path='/cart' element={isLoggedIn ? <Cart /> : <Navigate to="/" replace />} />
+        <Route path='/order' element={isLoggedIn ? <Placeorder /> : <Navigate to="/" replace />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -14,6 +14,7 @@ const Navbar = () => {
       <img src={assets.logo} alt="" className="logo"></img>
       <ul className='navbar-menu'>
         <Link to='/home' onClick={() => setMenu('home')} className={menu === 'home' ? 'active' : ''} >home</Link>
+        <Link to='/menu' onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ''} >Menu Registration</Link>
         <a href='#explore-menu' onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ''} >Menu</a>
         <li onClick={() => setMenu('mobile-app')} className={menu === 'mobile-app' ? 'active' : ''} >mobile-app</li>
         <li onClick={() => setMenu('contact us')} className={menu === 'contact us' ? 'active' : ''} >contact-us</li>
