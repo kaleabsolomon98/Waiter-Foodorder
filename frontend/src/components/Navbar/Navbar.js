9d09c09f144068f14c40069c25c14cccf64a1a1f
kaleab solomon <kaleabsolomon98@gmail.com>
2024-10-26 12:27:12 +0300

    Editing nav bar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,25 +1,38 @@
 import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/frontend_assets/assets';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
+import { useEffect } from 'react';
 
 
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   var [menu, setMenu] = useState("home");
 
-  const { getTotalCartAmount } = useContext(StoreContext);
+  const { getTotalCartAmount, setIsLoggedIn } = useContext(StoreContext);
+
+  useEffect(() => {
+    // Scroll to #explore-menu when on the home page and menu is selected
+    if (location.pathname === '/home' && menu === 'menu') {
+      const element = document.getElementById('explore-menu');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location, menu]); 
 
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo"></img>
       <ul className='navbar-menu'>
         <Link to='/home' onClick={() => setMenu('home')} className={menu === 'home' ? 'active' : ''} >home</Link>
-        <a href='#explore-menu' onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ''} >Menu</a>
+        <Link to='/home' onClick={() => setMenu('menu')}  className={menu === 'menu' ? 'active' : ''} >Menu</Link>
+        {/* <a href='#explore-menu' onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ''} >Menu</a> */}
         <Link to='/category' onClick={() => setMenu('category')} className={menu === 'category' ? 'active' : ''} >Category</Link>
         <Link to='/subcategory' onClick={() => setMenu('subCategory')} className={menu === 'subCategory' ? 'active' : ''} >Sub Category</Link>
         <Link to='/menu' onClick={() => setMenu('addMenu')} className={menu === 'addMenu' ? 'active' : ''} >Add Menu</Link>
@@ -31,7 +44,11 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt='' />
           <div className={getTotalCartAmount() === 0 ? '' : 'dot'}></div>
         </div>
-        <button onClick={() => navigate('/', { replace: true })}>Sign Out</button>
+        <button onClick={() => {
+          console.log("--------PRINT LOGGING OUT--------");
+          setIsLoggedIn(false);
+          navigate('/', { replace: true });
+        }}>Sign Out</button>
       </div>
     </div>
   )
