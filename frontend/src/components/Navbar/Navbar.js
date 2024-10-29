d81ad055f012b0d882892792490f777a281073f9
kaleab solomon <kaleabsolomon98@gmail.com>
2024-10-29 16:33:26 +0300

    Adding docker file to the baackend

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,21 +1,16 @@
-import React, { useContext, useState, useRef } from 'react'
-import './Navbar.css'
+import React, { useContext, useState, useRef, useEffect } from 'react';
+import './Navbar.css';
 import { assets } from '../../assets/frontend_assets/assets';
 import { Link, useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
-import { useEffect } from 'react';
-
-
 
 const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  var [menu, setMenu] = useState("home");
-
+  const [menu, setMenu] = useState("home");
   const { getTotalCartAmount, setIsLoggedIn, fetchFoodItems } = useContext(StoreContext);
-
   const previousPath = useRef(location.pathname);
 
   useEffect(() => {
@@ -31,9 +26,15 @@ const Navbar = () => {
   useEffect(() => {
     const isNavigatingToHome = location.pathname === '/home' && previousPath.current !== '/menu';
     const isNavigatingToMenu = location.pathname === '/menu' && previousPath.current !== '/home';
+    const isNavigatingToCart = location.pathname === '/cart';
 
     if (isNavigatingToHome || isNavigatingToMenu) {
       fetchFoodItems();
+    }
+
+    // Reset menu when navigating to cart
+    if (isNavigatingToCart) {
+      setMenu(''); // or set it to a default value like 'home'
     }
 
     // Update the previous path
@@ -44,28 +45,24 @@ const Navbar = () => {
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo"></img>
       <ul className='navbar-menu'>
-        <Link to='/home' onClick={() => setMenu('home')} className={menu === 'home' ? 'active' : ''} >home</Link>
-        <Link to='/home' onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ''} >Menu</Link>
-        {/* <a href='#explore-menu' onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ''} >Menu</a> */}
-        <Link to='/category' onClick={() => setMenu('category')} className={menu === 'category' ? 'active' : ''} >Category</Link>
-        <Link to='/subcategory' onClick={() => setMenu('subCategory')} className={menu === 'subCategory' ? 'active' : ''} >Sub Category</Link>
-        <Link to='/menu' onClick={() => setMenu('addMenu')} className={menu === 'addMenu' ? 'active' : ''} >Add Menu</Link>
-
+        <Link to='/home' onClick={() => setMenu('home')} className={menu === 'home' ? 'active' : ''}>Home</Link>
+        <Link to='/home' onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ''}>Menu</Link>
+        <Link to='/category' onClick={() => setMenu('category')} className={menu === 'category' ? 'active' : ''}>Category</Link>
+        <Link to='/subcategory' onClick={() => setMenu('subCategory')} className={menu === 'subCategory' ? 'active' : ''}>Sub Category</Link>
+        <Link to='/menu' onClick={() => setMenu('addMenu')} className={menu === 'addMenu' ? 'active' : ''}>Add Menu</Link>
       </ul>
       <div className="navbar-right">
-        {/* <img src={assets.search_icon} alt="" /> */}
         <div onClick={() => navigate('/cart', { replace: true })} className='navbar-search-icon'>
           <img src={assets.basket_icon} alt='' />
           <div className={getTotalCartAmount() === 0 ? '' : 'dot'}></div>
         </div>
         <button onClick={() => {
-          console.log("--------PRINT LOGGING OUT--------");
           setIsLoggedIn(false);
           navigate('/', { replace: true });
         }}>Sign Out</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
